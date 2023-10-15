1bc6cae413c9a79024064eab45094a54ac4b1e14
Velmurugan Velayutham <velayutham@juniper.net>
2023-10-15 09:20:14 +0530

    added clerk authentication

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 import Home from './Home';
-import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useNavigate, Navigate } from "react-router-dom";
 import AddCustomer from './AddCustomer';
 import NavBar from './NavBar';
 import Footer from './Footer';
@@ -11,21 +11,127 @@ import AddCityName from './AddCityName';
 import AddLineMan from './AddLineMan';
 import AddReceipt from './Receipt';
 import Ledger from './Reports/Ledger';
+
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignUp,
+  UserButton,
+  SignIn,
+  useUser,
+  ClerkLoading,
+  RedirectToSignIn,
+} from "@clerk/clerk-react";
+if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+
+function ClerkProviderWithRoutes() {
+  const navigate = useNavigate();
+
+  return (
+    <ClerkProvider
+      publishableKey={clerkPubKey}
+      navigate={(to) => navigate(to)}
+    >
+      <Routes>
+        <Route
+          path="/create"
+          element={
+            <>
+              <ClerkLoading>
+                <div>Clerk is loading</div>
+              </ClerkLoading>
+              <SignedIn>
+                <AddCustomer />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
+          }
+        />
+        <Route
+          path="/citycreate"
+          element={
+            <>
+              <SignedIn>
+                <AddCityName />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
+          }
+        />
+        <Route
+          path="/linemancreate"
+          element={
+            <>
+              <SignedIn>
+                <AddLineMan />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
+          }
+        />
+        <Route
+          path="/loan"
+          element={
+            <>
+              <SignedIn>
+                <LoanForm />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
+          }
+        />
+        <Route
+          path="/receipt"
+          element={
+            <>
+              <SignedIn>
+                <AddReceipt />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
+          }
+        />
+        <Route
+          path="/ledger"
+          element={
+            <>
+              <SignedIn>
+                <Ledger />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
+          }
+        />
+        <Route index element={<Navigate to="/create" />} />
+      </Routes>
+
+    </ClerkProvider>
+  );
+}
+
 function App() {
   return (
     <div className="app">
       <React.Fragment >
         <NavBar />
-          <Router basename="/loan-app"  >
-            <Routes >
-                <Route path='/' element={<Home />} />
-                <Route path='/create' element={<AddCustomer />} />
-                <Route path='/citycreate' element={<AddCityName/>}/>
-                <Route path='/linemancreate' element={<AddLineMan/>}/>
-                <Route path='/loan' element={<LoanForm />} />
-                <Route path='/receipt' element={< AddReceipt/>} />
-                <Route path='/ledger' element={< Ledger/>} />
-            </Routes>
+        <Router basename="/loan-app" >
+          <ClerkProviderWithRoutes />
         </Router>
         <Footer />
       </React.Fragment>
