446829150bb9458a887a2a37e204c2192d06f32b
murugeswari <murugeswarivellilingam@gmail.com>
2025-02-02 14:16:57 +0530

    Chart size is bigger changed

--- src/Reports/Charrt.js
+++ src/Reports/Charrt.js
@@ -168,7 +168,7 @@ function Charrt({ loanno, ledger, company, date }) {
 
                                 arr1 && arr1.length > 0
                                     ?
-                                    (arr1.slice(0, arr1.length).map((i) => {
+                                    (arr1.slice(0, arr1.length).map((arr1) => {
                                         serialno = serialno + 1;
                                         totalamount = totalamount - first.dueamount;
                                         if (serialno == 1) {
